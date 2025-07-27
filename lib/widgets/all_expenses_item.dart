2fd4f15480577383d1484f0edfcae774d9d6c39a
Hussein Rizk <husseinrizk117@gmail.com>
2025-07-27 15:08:03 +0300

    create all expenses item header

--- lib/widgets/all_expenses_item.dart
+++ lib/widgets/all_expenses_item.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+import 'all_expensess_item_header.dart';
+
+class AllExpensesItem extends StatelessWidget {
+  const AllExpensesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      decoration: ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            width: 1,
+            color: Color(0xFFF1F1F1),
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Column(
+        children: [AllExpensessItemHeader(image: '')],
+      ),
+    );
   }
 }
